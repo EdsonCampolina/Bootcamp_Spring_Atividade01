7af9b463a84c0acbc8fa908de5c0673fd8c53e89
EdsonCampolina <ejcsengsoft@gmail.com>
2023-10-29 21:16:28 -0300

    Criando aplicação

---
+++ atividadeUm/src/main/java/com/atividadeUm/atividadeUm/AtividadeUmApplication.java
@@ -0,0 +1,13 @@
+package com.atividadeUm.atividadeUm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeUmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeUmApplication.class, args);
+	}
+
+}
