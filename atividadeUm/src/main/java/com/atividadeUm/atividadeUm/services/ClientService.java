dd4231becb79012d55ac491197535a61084a33f9
EdsonCampolina <ejcsengsoft@gmail.com>
2023-10-30 21:42:00 -0300

    Terminando a atividade

--- atividadeUm/src/main/java/com/atividadeUm/atividadeUm/services/ClientService.java
+++ atividadeUm/src/main/java/com/atividadeUm/atividadeUm/services/ClientService.java
@@ -1,15 +1,21 @@
 package com.atividadeUm.atividadeUm.services;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.atividadeUm.atividadeUm.dto.ClientDTO;
 import com.atividadeUm.atividadeUm.entities.Client;
 import com.atividadeUm.atividadeUm.repositories.ClientRepository;
+import com.atividadeUm.atividadeUm.services.exceptions.DatabaseException;
 import com.atividadeUm.atividadeUm.services.exceptions.ResourceNotFoundException;
 
 import jakarta.persistence.EntityNotFoundException;
@@ -21,8 +27,9 @@ public class ClientService {
 	private ClientRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<ClientDTO> findAll() {
-		return repository.findAll().stream().map(x -> new ClientDTO(x)).toList();
+
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
+		return repository.findAll(pageRequest).map(x -> new ClientDTO(x));
 	}
 
 	@Transactional(readOnly = true)
@@ -52,6 +59,16 @@ public class ClientService {
 			return new ClientDTO(entity);
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		} catch (DataIntegrityViolationException err) {
+			throw new DatabaseException("Integrity violation");
 		}
 	}
 
